53180cde0ec5442fcb836504bd1795b21b98baf2
amagana8 <alexander.jm28@gmail.com>
2022-05-30 11:59:32 -0500

    cleanup apollo server

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -27,28 +27,9 @@ const ogm = new OGM({
   },
 });
 
-async function startOgm() {
-  await ogm.init();
-}
-startOgm();
-
 export const User = ogm.model('User');
 
 export default cors(async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  // res.setHeader(
-  //   'Access-Control-Allow-Origin',
-  //   'https://studio.apollographql.com',
-  // );
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
-  );
-  if (req.method === 'OPTIONS') {
-    res.end();
-    return false;
-  }
-
   const neoSchema = new Neo4jGraphQL({
     typeDefs,
     driver,
@@ -60,12 +41,9 @@ export default cors(async (req, res) => {
 
   const apolloServer = new ApolloServer({
     schema: await neoSchema.getSchema(),
-    context: ({ req }) => {
-      return {
-        req,
-      };
-    },
+    context: ({ req }) => ({ req }),
   });
+  await ogm.init();
   await apolloServer.start();
   await apolloServer.createHandler({
     path: '/api/graphql',
