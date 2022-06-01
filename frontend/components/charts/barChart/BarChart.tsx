13574145f5c9613c72c654d4cb0b7de3e1d2a586
amagana8 <alexander.jm28@gmail.com>
2022-06-01 14:05:47 -0500

    fixed bar chart bug

--- frontend/components/charts/barChart/BarChart.tsx
+++ frontend/components/charts/barChart/BarChart.tsx
@@ -2,6 +2,7 @@ import { Column } from '@ant-design/plots';
 import styles from './BarChart.module.scss';
 import { Empty } from 'antd';
 import { colorMap } from '@utils/enums';
+import { useEffect, useState } from 'react';
 
 interface BarChartProps {
   data: ScoreData[];
@@ -12,12 +13,30 @@ interface ScoreData {
   amount: number;
 }
 
+interface BarChartEntry {
+  score: string;
+  amount: number;
+}
+
 const BarChart = ({ data }: BarChartProps) => {
+  const [barData, setBarData] = useState<BarChartEntry[]>([]);
+  useEffect(() => {
+    const newData = [];
+    for (let i = 1; i < 11; i++) {
+      const currItem = data.find((item) => item.score === i);
+      if (currItem) {
+        newData.push({ score: currItem.score.toString(), amount: currItem.amount });
+      } else {
+        newData.push({ score: i.toString(), amount: 0 });
+      }
+    }
+    setBarData(newData);
+  }, [data]);
   const config = {
     color: ({ score }: any) => {
       return colorMap.get(Number(score)) ?? '#177ddc';
     },
-    data,
+    data: barData,
     xField: 'score',
     yField: 'amount',
     label: {
