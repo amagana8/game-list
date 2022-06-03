0361eeed5e5fe648f68449b4ca2549a849d2f817
amagana8 <alexander.jm28@gmail.com>
2022-06-03 13:29:58 -0500

    added nprogress bar when navigating between pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,40 @@ import { Provider } from 'react-redux';
 import { store, persistor } from '@frontend/store';
 import { client } from '@frontend/apollo-client';
 import { PersistGate } from 'redux-persist/integration/react';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+NProgress.configure({
+  minimum: 0.3,
+  easing: 'ease',
+  speed: 500,
+  showSpinner: false,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = () => {
+      NProgress.start();
+    };
+    const handleStop = () => {
+      NProgress.done();
+    };
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+  }, [router]);
+
   return (
     <>
       <ApolloProvider client={client}>
