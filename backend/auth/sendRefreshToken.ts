b09d7148ba5858e0f8cbfd94adba859e635ef22e
amagana8 <alexander.jm28@gmail.com>
2022-06-14 09:48:55 -0500

    fixed issue with refresh token cookie

--- backend/auth/sendRefreshToken.ts
+++ backend/auth/sendRefreshToken.ts
@@ -2,6 +2,6 @@ import { NextApiResponse } from 'next';
 
 export const sendRefreshToken = (res: NextApiResponse, token: string) => {
   res.setHeader('Set-Cookie', [
-    `refresh-token=${token}; Max-Age=${60 * 60 * 24 * 7}; HttpOnly`,
+    `refreshToken=${token}; Max-Age=${60 * 60 * 24 * 7}; Path=/; HttpOnly`,
   ]);
 };
